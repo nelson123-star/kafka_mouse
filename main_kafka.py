15515a959c6bdd6e975eb2668d6108ab47841b20
nelson123-star <vl301297@gmail.com>
2026-05-07 22:23:53 +0700

    убрал try except в цикле

--- main_kafka.py
+++ main_kafka.py
@@ -26,30 +26,28 @@ if __name__ == '__main__':
     start_time = time.time()
     
     try:
-    
         while True:
         
-            try:
-                x, y = pyautogui.position()
-                print(f"x_coordinates: {x}, y_coordinates: {y}")
-                producer.send(KAFKA_TOPIC, {'x_coordinates': x, 'y_coordinates': y})
-                producer.flush()
-                
-                
-                now = time.time()
-                print(f"Время выполнения: {now - start_time}")
-                if now - start_time >= TIME_CHECK:
-                    print(f"{TIME_CHECK} секунд истекло, выход из цикла")
-                    break
-                
-                time.sleep(1)  # Пауза 1 сек
-                
-            except KafkaError as e:
-                print(f"Ошибка Kafka: {e}")
+            x, y = pyautogui.position()
+            print(f"x_coordinates: {x}, y_coordinates: {y}")
+            producer.send(KAFKA_TOPIC, {'x_coordinates': x, 'y_coordinates': y})
+            producer.flush()
+            
+            
+            now = time.time()
+            print(f"Время выполнения: {now - start_time}")
+            if now - start_time >= TIME_CHECK:
+                print(f"{TIME_CHECK} секунд истекло, выход из цикла")
                 break
-            except Exception as e:
-                print(f"Ошибка: {e}")
-                break
+            
+            # Пауза 1 сек
+            time.sleep(1)  
+                
+    except KafkaError as e:
+        print(f"Ошибка Kafka: {e}")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
     
     finally:
         producer.close()
